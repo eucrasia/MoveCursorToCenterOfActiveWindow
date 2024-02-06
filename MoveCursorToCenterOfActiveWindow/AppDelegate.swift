5cdc205c6d5d0f004adc8865ae7395ada9079072
shunta <shunta@eucr.asia>
2024-02-07 00:25:33 +0900

    Add the debug logging feature using the Logger API
    The app now requires macOS 11.0+

--- MoveCursorToCenterOfActiveWindow/AppDelegate.swift
+++ MoveCursorToCenterOfActiveWindow/AppDelegate.swift
@@ -5,31 +5,62 @@
 
 import Cocoa
 import CoreGraphics
+import OSLog
 
-// @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    var window: NSWindow!
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        if
-            let windowList = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID) as? [NSDictionary],
-            let activeWindowInfo = windowList.first(where: { $0[kCGWindowLayer] as? Int == 0
-            }),
-            let activeWindowBounds = activeWindowInfo[kCGWindowBounds] as? NSDictionary,
-            let rect = CGRect(dictionaryRepresentation: activeWindowBounds)
-        {
-            // debugPrint(rect)
-            let centerOfActiveWindow = CGPoint(x: rect.midX, y: rect.midY)
-            
-            let error = CGWarpMouseCursorPosition(centerOfActiveWindow)
-            if error == CGError.success {
-                exit(EX_OK)
-            } else {
-                debugPrint(error)
-                exit(EX_UNAVAILABLE)
-            }
+        
+        //
+        // Usage: /path/to/MoveCursorToCenterOfActiveWindow.app [--debug]
+        //
+        let debugMode = CommandLine.arguments.contains("--debug")
+        
+        let bundleID = Bundle.main.bundleIdentifier ?? ""
+        let category = OSLog.Category.pointsOfInterest.rawValue
+        let logger = Logger(subsystem: bundleID, category: category)
+        
+        // We don't use Logger log levels below the default one
+        // (because logs having those levels don't persist after the app exits, and this app exits rather quickly)
+        
+        if debugMode {
+            logger.notice("[DEBUG] Screens have separate spaces: \(NSScreen.screensHaveSeparateSpaces, privacy: .public)")
+            logger.notice("[DEBUG] Screens: \(NSScreen.screens, privacy: .public)")
+        }
+        
+        guard let windowList = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID) as? [NSDictionary] else {
+            debugMode ? logger.error("[DEBUG] Cannot retrieve the window list") : logger.error("Cannot retrieve the window list")
+            exit(EX_UNAVAILABLE)
+        }
+        
+        guard let activeWindowInfo = windowList.first(where: { $0[kCGWindowLayer] as? Int == 0 }) else {
+            debugMode ? logger.notice("[DEBUG] No active window found") : logger.notice("No active window found")
+            exit(EX_NOINPUT)
+        }
+        
+        guard let activeWindowBounds = activeWindowInfo[kCGWindowBounds] as? NSDictionary,
+              let rect = CGRect(dictionaryRepresentation: activeWindowBounds) else {
+            if debugMode {
+                logger.error("[DEBUG] Cannot retrieve the bounds of the active window: \(activeWindowInfo, privacy: .public)")
+            } else { logger.error("Cannot retrieve the bounds of the active window") }
+            exit(EX_UNAVAILABLE)
+        }
+        
+        let centerOfActiveWindow = CGPoint(x: rect.midX, y: rect.midY)
+        
+        if debugMode {
+            logger.notice("[DEBUG] Active window: \(activeWindowInfo, privacy: .public)")
+            logger.notice("[DEBUG] Attempting to move the cursor to \(centerOfActiveWindow.debugDescription, privacy: .public)")
+        }
+        let cgError = CGWarpMouseCursorPosition(centerOfActiveWindow)
+        
+        if cgError == CGError.success {
+            if debugMode { logger.notice("[DEBUG] Successfully moved the cursor") }
+            exit(EX_OK)
         } else {
+            if debugMode {
+                logger.error("[DEBUG] Unable to move the cursor: \(String(describing: cgError), privacy: .public)")
+            } else { logger.error("Unable to move the cursor") }
             exit(EX_UNAVAILABLE)
         }
     }
@@ -38,4 +69,3 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 }
-
